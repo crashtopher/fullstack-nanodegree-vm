4b88ea2a2b035fcd03a10f33ed7c765e57e48328
crashtopher <christopher.shaw193@gmail.com>
2016-10-20 15:09:21 -0700

    connect forum to database and display/add posts

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,29 +3,23 @@
 #
 
 import time
-
-## Database connection
-DB = [psycopg2.connect("dbname=forum")]
+import psycopg2
 
 ## Get posts from database.
 def GetAllPosts():
-    '''Get all the posts from the database, sorted with the newest first.
-
-    Returns:
-      A list of dictionaries, where each dictionary has a 'content' key
-      pointing to the post content, and 'time' key pointing to the time
-      it was posted.
-    '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    c.execute('SELECT time, conent FROM posts ORDER BY time DESC')
+    posts = ({'conent': str(row[1]), 'time': str(row[0])}
+             for row in c.fetchall())
+    DB.close
     return posts
 
 ## Add a post to the database.
-def AddPost(content):
-    '''Add a new post to the database.
 
-    Args:
-      content: The text content of the new post.
-    '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+def AddPost(content):
+    DB = DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    c.execute('INSERT INTO posts (content) VALUES (%s)' %content)
+    DB.commit()
+    DB.close
