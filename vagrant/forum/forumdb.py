fa4ef0bfa1f661d0fc1aff65142ceb14ef8987a5
crashtopher <christopher.shaw193@gmail.com>
2016-10-21 08:27:36 -0700

    complete project

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -4,13 +4,14 @@
 
 import time
 import psycopg2
+import bleach
 
 ## Get posts from database.
 def GetAllPosts():
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
-    c.execute('SELECT time, conent FROM posts ORDER BY time DESC')
-    posts = ({'conent': str(row[1]), 'time': str(row[0])}
+    c.execute('SELECT time, content FROM posts ORDER BY time DESC')
+    posts = ({'content': str(bleach.clean(row[1])), 'time': str(row[0])}
              for row in c.fetchall())
     DB.close
     return posts
@@ -18,8 +19,8 @@ def GetAllPosts():
 ## Add a post to the database.
 
 def AddPost(content):
-    DB = DB = psycopg2.connect("dbname=forum")
+    DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
-    c.execute('INSERT INTO posts (content) VALUES (%s)' %content)
+    c.execute('INSERT INTO posts (content) VALUES (%s)', (content,))
     DB.commit()
     DB.close
