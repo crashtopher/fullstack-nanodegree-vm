c049659b38889320f271687bba19970a753cc1d8
crashtopher <christopher.shaw193@gmail.com>
2016-10-22 11:39:13 -0700

    update tournament.sql to smoothen out import of file. D.R.Y. tournament.py using contextlib

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -4,40 +4,55 @@
 #
 
 import psycopg2
+from contextlib import contextmanager
 
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+    try:
+        return psycopg2.connect("dbname=tournament")
+    except:
+        print("Connection failed")
+
+
+@contextmanager
+def get_cursor():
+    """
+    Query helper function using context lib. Creates a cursor from a database
+    connection object, and performs queries using that cursor.
+    """
+    DB = connect()
+    c = DB.cursor()
+    try:
+        yield c
+    except:
+        raise
+    else:
+        DB.commit()
+    finally:
+        c.close()
+        DB.close()
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
-    conn = connect()
-    c = conn.cursor()
-    c.execute("UPDATE t_results Set wins = 0, matches = 0;")
-    conn.commit()
-    conn.close()
+    with get_cursor() as c:
+        c.execute("UPDATE t_results Set wins = 0, matches = 0;")
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    conn = connect()
-    c = conn.cursor()
-    c.execute("DELETE FROM t_results;")
-    c.execute("DELETE FROM players;")
-    conn.commit()
-    conn.close()
+    with get_cursor() as c:
+        c.execute("DELETE FROM t_results;")
+        c.execute("DELETE FROM players;")
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    conn = connect()
-    c = conn.cursor()
-    c.execute("SELECT COUNT(*) as num FROM players;")
-    val = c.fetchone()[0]
-    return val
-    conn.close()
+    with get_cursor() as c:
+        c.execute("SELECT COUNT(*) as num FROM players;")
+        val = c.fetchone()[0]
+        return val
 
 
 def registerPlayer(name):
@@ -48,16 +63,13 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
-    conn = connect()
-    c = conn.cursor()
-    c.execute("INSERT INTO players (name) Values (%s)", (name,))
-    conn.commit()
-    c.execute("SELECT id FROM players WHERE name = %s", (name,))
-    player_id = c.fetchone()
-    c.execute(
-        "INSERT INTO t_results (id, wins, matches) VALUES (%s, 0 ,0)", (player_id,))
-    conn.commit()
-    conn.close()
+    with get_cursor() as c:
+        c.execute("INSERT INTO players (name) Values (%s)", (name,))
+        c.execute("SELECT id FROM players WHERE name = %s", (name,))
+        player_id = c.fetchone()
+        c.execute(
+            """INSERT INTO t_results (id, wins, matches)
+               VALUES (%s, 0 ,0)""", (player_id,))
 
 
 def playerStandings():
@@ -73,14 +85,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-    conn = connect()
-    c = conn.cursor()
-    c.execute(
-        """SELECT players.id, name, wins, matches FROM t_results
-           LEFT JOIN players ON players.id=t_results.id ORDER BY wins DESC;""")
-    val = c.fetchall()
-    return val
-    conn.close()
+    with get_cursor() as c:
+        c.execute(
+            """SELECT players.id, name, wins, matches FROM t_results
+               LEFT JOIN players ON players.id=t_results.id ORDER BY wins DESC;""")
+        val = c.fetchall()
+        return val
 
 
 def reportMatch(winner, loser):
@@ -90,17 +100,13 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
-    conn = connect()
-    c = conn.cursor()
-    c.execute(
-        """UPDATE t_results set wins = wins+1, matches = matches+1
-           where t_results.id=%s;""", (winner,))
-    conn.commit()
-    c.execute(
-        """UPDATE t_results set matches = matches+1
-           where t_results.id=%s;""", (loser,))
-    conn.commit()
-    conn.close()
+    with get_cursor() as c:
+        c.execute(
+            """UPDATE t_results set wins = wins+1, matches = matches+1
+               where t_results.id=%s;""", (winner,))
+        c.execute(
+            """UPDATE t_results set matches = matches+1
+               where t_results.id=%s;""", (loser,))
 
 
 def swissPairings():
@@ -118,23 +124,21 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-    conn = connect()
-    c = conn.cursor()
-    c.execute(
-        """CREATE VIEW standings AS
-           SELECT players.id, name, wins, matches FROM t_results
-           LEFT JOIN players ON players.id=t_results.id ORDER BY wins DESC;""")
-    c.execute("SELECT id, name FROM standings;")
-    info = c.fetchall()
-    pairings = []
-    v1 = info[0] + info[1]
-    v2 = info[2] + info[3]
-    v3 = info[4] + info[5]
-    v4 = info[6] + info[7]
-    pairings.append(v1)
-    pairings.append(v2)
-    pairings.append(v3)
-    pairings.append(v4)
-    return pairings
-    c.execute("DROP VIEW standings")
-    conn.close()
+    with get_cursor() as c:
+        c.execute(
+            """CREATE VIEW standings AS
+               SELECT players.id, name, wins, matches FROM t_results
+               LEFT JOIN players ON players.id=t_results.id ORDER BY wins DESC;""")
+        c.execute("SELECT id, name FROM standings;")
+        info = c.fetchall()
+        c.execute("DROP VIEW standings")
+        pairings = []
+        v1 = info[0] + info[1]
+        v2 = info[2] + info[3]
+        v3 = info[4] + info[5]
+        v4 = info[6] + info[7]
+        pairings.append(v1)
+        pairings.append(v2)
+        pairings.append(v3)
+        pairings.append(v4)
+        return pairings
